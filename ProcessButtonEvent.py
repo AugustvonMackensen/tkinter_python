65c7f8d2249e249f742c24e7cbb1a60c4c41b630
Leo Lee <bonaparte_19th@hotmail.com>
2022-10-24 00:11:15 +0900

    button practice

--- ProcessButtonEvent.py
+++ ProcessButtonEvent.py
@@ -7,3 +7,10 @@ def processCancel():
     print('cancel 버튼이 클릭되었습니다.')
 
 window = Tk() # 창 생성
+btOK = Button(window, text='OK', fg='red', command=processOK)
+btCancel = Button(window, text='Cancel', fg='yellow', command=processCancel)
+
+btOK.pack() # ok버튼 배치
+btCancel.pack() # Cancel 버튼 창 내부 배치
+
+window.mainloop() # 이벤트 루프 생성.
